3bdd7da86bfdffb6da5780228bce7e404eae9424
Patrik Grinsvall <patrik@silentridge.io>
2021-11-14 05:49:34 +0100

    several small updates

--- src/Commands/ImportSpreadsheetCommand.php
+++ src/Commands/ImportSpreadsheetCommand.php
@@ -20,10 +20,11 @@ class ImportSpreadsheetCommand extends Command
     protected $signature = 'import:google-spreadsheet - Imports from google spreadsheet to db using a eloquent model, trying to match header with columns, its pretty fast, 3k rows in a few seconds.
                                 {--s|spreadsheet=   : The google docs spreadsheet id. The character sequence after /d/ in the spreadsheet URL. This must be made public (required)}
                                 {--m|model=         : The eloquent model to match. The columns in the spreadsheet must match the columns snake_case in the model or else they are silently skipped (required)}
-                                {--u|unique-key=    : eg. title=identifier, this will take header column in csv and match against the column identifier in the model which must be unique (optional)}
+                                {--u|unique-key=    : eg. header_column=column_name, this will match spreadsheet header column against the attribute/table column name in the model. Example is to use the name of the primary_key in the table. (optional)}
                                 {--j|json-column=   : If the model has a json column where all data should be put, specify this here. Theoreticly this option is the only one needed. (optional)}
                                 {--c|create-new     : Always create a new record, never match key and try to update the unique key. (boolean, optional)}
-                                {--f|filename=      : Use this filename as intermediate storage. No need to use unless you want to parse csv in another job. (default=import.csv)}
+                                {--f|filename=      : Use this filename as intermediate storage. No need to use unless you want to parse csv in another job. (default=storage_path('import.csv')}
+                                {--d|disable-cache  : By default the response from google docs is cached for an hour, this flag disables this cache}
                                 {--e|skip-errors    : Skip errors if possible and continue with next row, such as duplicate key or error when inserting data}
                                 ';
 
@@ -32,13 +33,16 @@ class ImportSpreadsheetCommand extends Command
      *
      * @var string
      */
-    protected $description = 'Import from google spreadsheets into a model.Note, in order to use the update existing row feature the model must have softdeletes.';
+    protected $description = 'Import from google spreadsheets into a model.Note, in order to use the update existing row feature the model must have soft-deletes enabled. Note! The response from google docs is by default cached for an hour but can be toggled with --disable-cache flag';
     protected $help = 'Examples:
-    - php artisan import:google-spreadsheet -s spreadsheetid --create-new --model=App\\\\Models\\\\Post --skip-errors --json-column=json_data
+    1) Always create a new record, use the ´Post´ model and dont match against existing record, continue even if there are database errors and also add all values to a json column named json_data without caching the response
+    - php artisan import:google-spreadsheet -s spreadsheetid --create-new --model=App\\\\Models\\\\Post --skip-errors --json-column=json_data --disable-cache
+    2) Always create a new record, use the ´Post´ model and dont match against existing record, save the spreadsheet in ´storage_path("mytmpcsv.csv")´
     - php artisan import:google-spreadsheet -s spreadsheetid --create-new --model=App\\\\Models\\\\Post --filename=mytmpcsv.csv
-    - php artisan import:google-spreadsheet -s spreadsheetid -u="Spreadsheet header column=datebase_column"  --model=App\\\\Models\\\\Post --json-column=post_attributes
+    3) Import data using ´database_column´ as unique key, meaning all rows will be matched against database record with this corresponding value from csv, use the ´Post´ model, save all fields as json in column ´post_attributes´ and dont cache the response
+    - php artisan import:google-spreadsheet -s spreadsheetid -u="Spreadsheet header column=datebase_column"  --model=App\\\\Models\\\\Post --json-column=post_attributes --disable-cache
 
-    Config can be published and also set in environment variables. Handy if supposed to syncronize data from a spreadsheet into database on a regular basis.
+    The config can be published using ´artisan vendor:publish´. The config values can also be set as environment variables with the same name but UPPER_CASED. Handy if you want to syncronize data from a spreadsheet into database on a regular basis, for example as a cron-job.
     ';
     public $model;
     public $uniqueAttribute = "identifier";
@@ -48,6 +52,7 @@ class ImportSpreadsheetCommand extends Command
     public $baseUrl = "https://docs.google.com/spreadsheets/d/";
     public $filename = "csvimport.csv";
     public $json_column;
+    public $cacheTtl = 3600;
 
     /**
      * Execute the console command.
@@ -146,17 +151,24 @@ class ImportSpreadsheetCommand extends Command
         if (blank($this->option('model') && !config($this->confNamespace . '.model'))) {
             $this->error('Missing what model to use');
         }
+        $this->cacheTtl = $this->option('cache-ttl') ?? config($this->confNamespace . ".cache-ttl") ?? $this->cacheTtl;
 
-
-        $this->model = app($this->option('model'));
+        $this->model = app($this->option('model')) ?? config($this->confNamespace . '.model');
         throw_if(!in_array('Illuminate\Database\Eloquent\SoftDeletes', class_uses($this->model)),
             "The model doesnt have soft deletes which are required. Please add softdeletes to the model or override importcsv function");
 
-        $tmp = explode("=", $this->option("unique-key") ?? config($this->confNamespace . ".unique-key"));
+        if(!is_null($this->uniqueCol)) {
+            $tmp = explode("=", $this->option("unique-key") ?? config($this->confNamespace . ".unique-key"));
 
-        $this->uniqueCol = $tmp[0];
-        $this->uniqueAttribute = $tmp[1];
+            $this->uniqueCol = $tmp[0];
+            $this->uniqueAttribute = $tmp[1];
+            throw_if(!array_key_exists($this->uniqueAttribute, $this->model->getAttributes()), "The attribute: ". $this->uniqueAttribute." was not found in model: ".$this->model::class)
+        }
         $this->json_column = $this->option('json-column') ?? null;
+        if(!is_null($this->json_column)) {
+            throw_if(!array_key_exists($this->json_column, $this->model->getAttributes()), "The json column: ". $this->json_column." was not found in model: ".$this->model::class)
+        }
+
     }
 
 }
